d6f653a69699bc82601824d4b6654fc58d86e4a9
Tim Yoon <stimyoon@gmail.com>
2022-07-24 15:12:49 -0400

    Initial Commit

---
+++ World/WorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorldApp.swift
+//  World
+//
+//  Created by Tim Yoon on 7/24/22.
+//
+
+import SwiftUI
+
+@main
+struct WorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
