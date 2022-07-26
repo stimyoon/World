7ea8e999012eea257bb94bb050f7105bf5d2a77c
Tim Yoon <stimyoon@gmail.com>
2022-07-25 21:38:54 -0400

    First committee

--- World/ContentView.swift
+++ World/ContentView.swift
@@ -6,11 +6,88 @@
 //
 
 import SwiftUI
+protocol Locatable {
+    var row : Int { set get }
+    var col : Int { set get }
+    func moveTo(row: Int, col: Int)
+}
+extension Locatable {
+    mutating func moveTo(row: Int, col: Int){
+        let r = abs(row) % Constants.rowMax
+        let c = abs(col) % Constants.colMax
+        self.row = r
+        self.col = c
+    }
+}
 
-struct ContentView: View {
+struct MobileEntity : Identifiable{
+    var id = UUID()
+    var row: Int = 0
+    var col: Int = 0
+    private (set) var hp = 0
+    mutating func setHp(to newhp: Int) {
+        hp = newhp
+    }
+    mutating func moveTo(row: Int, col: Int){
+        let r = abs(row) % Constants.rowMax
+        let c = abs(col) % Constants.colMax
+        self.row = r
+        self.col = c
+    }
+    init(){
+        row = 0
+        col = 0
+    }
+}
+
+struct Constants {
+    static var rowMax = 9
+    static var colMax = 5
+}
+struct Locus : Identifiable {
+    var id = UUID()
+    var entities = [MobileEntity]()
+}
+
+class WorldVM : ObservableObject {
+    var grid : [[Locus]] = Array(repeating: Array(repeating: Locus(), count: Constants.colMax), count: Constants.rowMax)
+    init(){
+        grid[1][1].entities.append(MobileEntity())
+    }
+}
+
+struct EntitiesView: View {
+    @State private var entities : [MobileEntity]
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        HStack {
+            ForEach(0..entities.count)
+        }
+    }
+}
+struct WorldView: View {
+    @ObservedObject var vm : WorldVM
+    var body: some View {
+        
+        VStack (spacing: 1){
+            ForEach(0..<Constants.rowMax, id: \.self){ row in
+                HStack (spacing: 1){
+                    ForEach(0..<Constants.colMax, id: \.self){ col in
+                        Rectangle()
+                            .overlay {
+                                
+                            }
+                    }
+                }
+            }
+        }
+        .ignoresSafeArea()
+        .background(.green)
+    }
+}
+struct ContentView: View {
+    @StateObject var vm = WorldVM()
+    var body: some View {
+        WorldView(vm: vm)
     }
 }
 
